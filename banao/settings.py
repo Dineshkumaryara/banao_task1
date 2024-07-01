63348770a456ab005bc2bc86735a08931c852672
Dineshkumaryara <2100031355@kluniversity.in>
2024-07-01 15:54:32 +0530

    added files

--- banao/settings.py
+++ banao/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = "banao.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'banao',
-        'USER': 'root',
-        'PASSWORD': 'zaq1mlp0',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'banao',
+#         'USER': 'root',
+#         'PASSWORD': 'zaq1mlp0',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
