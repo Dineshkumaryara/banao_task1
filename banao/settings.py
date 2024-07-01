5bd6e422034b94b278bfb26dabd4b14a2ee4ede3
Dineshkumaryara <2100031355@kluniversity.in>
2024-07-01 16:13:21 +0530

    host

--- banao/settings.py
+++ banao/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6!855+acrfu9xmmkoco)s@iklxdr6yvkyjq*=!9@a#-of+dos9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
